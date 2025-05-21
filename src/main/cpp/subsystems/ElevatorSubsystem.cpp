5bfa0faefc1c1ee9a977f6b3a429f8d31412445a
Nithishsaran <Nithishsaran.K.S@gmail.com>
2025-05-21 18:48:39 -0400

    feat: Moving files

---
+++ src/main/cpp/subsystems/ElevatorSubsystem.cpp
@@ -0,0 +1,6 @@
+#include <include/subkElevatorSubsystem.h>
+
+ElevatorSubsystem::ElevatorSubsystem() = default;
+
+// This method will be called once per scheduler run
+void ElevatorSubsystem::Periodic() {}
--- src/main/include/subsystems/ElevatorSubsystem.h
+++ src/main/include/subsystems/ElevatorSubsystem.h
@@ -1,8 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
-#pragma once
+#ifndef ELEVATORSUBSYSTEM_H
 
 #include <frc2/command/SubsystemBase.h>
 
@@ -19,3 +15,5 @@ class ElevatorSubsystem : public frc2::SubsystemBase {
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
 };
+
+#define ELEVATORSUBSYSTEM_H
